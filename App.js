b2b3c0a74b1c75fada154f53371bd77ff5bfef98
Joao Vitor de Lira <joao.lira@scge.local>
2024-03-04 16:48:27 -0300

    só falta o css

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
-import { Button, Input, Icon } from '@rneui/themed'
+import { Button, Input } from '@rneui/themed'
+import { PlusCircleOutlined, ArrowLeftOutlined } from '@ant-design/icons'
+
+
 
 const login = () => {
   return (
@@ -39,22 +42,21 @@ const login = () => {
 
 const contatos = () => {
   return (
-    <View style={[styles.contatos]}>
+    <View style={[styles.geral]}>
 
       <View>
         <Text>Lista de contatos</Text>
-        <Icon
-        name='pluscircleo' />
+        <PlusCircleOutlined />
       </View>
 
       <View>
         <Image
           source={{ uri: 'https://cdn-icons-png.flaticon.com/512/5987/5987462.png' }}
           style={{ width: 100, height: 100 }}
-          
-        /> 
-          <Text>Marcos Andrade</Text>
-          <Text>81 988553424</Text>
+
+        />
+        <Text>Marcos Andrade</Text>
+        <Text>81 988553424</Text>
       </View>
 
       <View>
@@ -63,8 +65,8 @@ const contatos = () => {
           style={{ width: 100, height: 100 }}
         />
         <Text>Patrícia Tavares</Text>
-          <Text>81 998765332</Text>
-        
+        <Text>81 998765332</Text>
+
       </View>
 
       <View>
@@ -75,17 +77,122 @@ const contatos = () => {
       </View>
 
       <Text>Rodrigo Antunes</Text>
-          <Text>81 987765525</Text>
+      <Text>81 987765525</Text>
 
     </View>
   )
 }
 
+const cadUser = () => {
+
+  return (
+
+    <View style={[styles.geral]}>
+
+      <View>
+
+        <ArrowLeftOutlined/>
+        <Text>Usuário</Text>
+
+      </View>
+
+      <View>
+        <Text>Nome</Text>
+        <Input></Input>
+        <Text>cpf</Text>
+        <Input></Input>
+
+        <Text>email</Text>
+        <Input></Input>
+
+        <Text>senha</Text>
+        <Input></Input>
+
+        <Button>Salvar</Button>
+      </View>
+
+    </View>
+
+
+  )
+}
+
+const cadContato = () => {
+
+  return (
+
+    <View style={[styles.geral]}>
+
+      <View>
+
+        <ArrowLeftOutlined/>
+        <Text>Contato</Text>
+
+      </View>
+
+      <View>
+        <Text>Nome</Text>
+        <Input></Input>
+        <Text>Email</Text>
+        <Input></Input>
+
+        <Text>Telefone</Text>
+        <Input></Input>
+
+        <Button>Salvar</Button>
+      </View>
+
+    </View>
+
+
+  )
+}
+
+const alterContato = () => {
+
+  return (
+
+    <View style={[styles.geral]}>
+
+      <View>
+
+        <ArrowLeftOutlined/>
+        <Text>Contato</Text>
+
+      </View>
+
+      <View>
+        <Text>Nome</Text>
+        <Input value='Marco Andrade'></Input>
+        <Text>Email</Text>
+        <Input value='mand@gmail.com'></Input>
+
+        <Text>Telefone</Text>
+        <Input value='81 988553424'></Input>
+
+        <Button>Alterar</Button>
+        <Button>Excluir</Button>
+      </View>
+
+    </View>
+
+
+  )
+}
+
 const styles = StyleSheet.create({
-  login: {
+  geral: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  login: {
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffff0',
   },
   contatos: {
 
@@ -94,6 +201,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
 
   },
+  cadUser: {
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
 })
 
-export default contatos
+export default login
